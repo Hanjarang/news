09bfdd4ae606dcf57ce0e0c594fb52eaa943969a
hanjarang <rkdsksehowl@naver.com>
2025-07-04 17:14:55 +0900

    누락 파일 재커밋

---
+++ src/main/java/com/example/news/dto/OAuth2UserInfo.java
@@ -0,0 +1,9 @@
+package com.example.news.dto;
+
+public interface OAuth2UserInfo {
+  String getProvider();
+  String getProviderId();
+  String getName();
+  String getEmail();
+  String getProfileImage();
+}
