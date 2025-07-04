936c9da02d2a1d28ac9a8369c646657b0006d90d
hanjarang <rkdsksehowl@naver.com>
2025-07-04 17:11:31 +0900

    누락 파일 커밋

--- src/main/java/com/example/news/dto/OAuth2UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.news.dto;
-
-public interface OAuth2UserInfo {
-    String getProvider();
-    String getProviderId();
-    String getName();
-    String getEmail();
-    String getProfileImage();
-} 
